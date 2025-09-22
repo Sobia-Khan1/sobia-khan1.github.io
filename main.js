2694816950fcebc226c4e579da21c21f932c3311
Sobia-Khan1 <sobia.khan1@ucalgary.ca>
2025-09-22 15:02:46 -0600

    Add Effect on Click
    Add sparkle effect on click on page title

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
+var x = 1
+
+function titleAnimation(event) {
+    // if (x%2 === 1)
+    //     document.getElementById("title-box").style.backgroundColor = "white";
+    // else
+    //     document.getElementById("title-box").style.backgroundColor = "#C3AED6";
+    // x++;
+    let title = document.getElementById("title-box");
+
+    for (i = 0; i < 10; i++) {
+        console.log("X:", event.pageX, " Y:", event.pageY);
+        let sparkle = document.createElement("sparkle");
+        sparkle.style.top = (event.pageY + Math.random()*50) + "px";
+        sparkle.style.left = (event.pageX + Math.random()*50) + "px";
+        title.appendChild(sparkle);
+
+        setTimeout(function(){sparkle.remove();}, 2000)
+
+    }
+}
 
 
 function openProjectPopUp(prjNum) {
